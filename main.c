0551e1bdbb9722450f55239af16c6e1e1363b641
Martin Labe <martin.labe@lirmm.fr>
2021-06-02 16:51:51 +0200

    adding bfs merging

--- main.c
+++ main.c
@@ -1,49 +1,107 @@
 #include <stdio.h>
 #include <igraph/igraph.h>
 
-void print_vector(const igraph_vector_t* vids)
+void vector_print(igraph_vector_t *v)
 {
-    unsigned n = igraph_vector_size(vids);
-    for (unsigned i = 0; i < n; i++)
-    {
-        printf("%u ", (unsigned)VECTOR(*vids)[i]);
+    long int i;
+    for (i = 0; i < igraph_vector_size(v); i++) {
+        printf(" %li", (long int) VECTOR(*v)[i]);
     }
     printf("\n");
 }
 
+void graph_print(const igraph_t *graph)
+{
+    igraph_vector_t vector;
+    igraph_vector_init(&vector, 0);
+    igraph_get_edgelist(graph, &vector, 0);
+    vector_print(&vector);
+}
+
+void merge(igraph_t *graph, igraph_vector_t *parents)
+{
+    /// XXX uses a lot of RAM by allocationg a vedges
+    igraph_vector_t vedges;
+    igraph_vector_init(&vedges, igraph_vector_size(parents) * 2);
+
+    long int it = 0;
+    for (long int i = 0; i < igraph_vector_size(parents); i++)
+    {
+        long int p_i = VECTOR(*parents)[i];
+        igraph_vector_t neighbors;
+        igraph_vector_init(&neighbors, 0);
+        igraph_neighbors(graph, &neighbors, i, IGRAPH_ALL);
+        unsigned stop = 0;
+        if (i == p_i)
+            continue;
+        for (long int j = 0; j < igraph_vector_size(&neighbors); j++)
+        {
+            if (p_i == VECTOR(neighbors)[j])
+            {
+                stop = 1;
+                break;
+            }
+        }
+        if (!stop)
+        {
+            VECTOR(vedges)[it] = i;
+            it += 1;
+            VECTOR(vedges)[it] = p_i;
+            it += 1;
+        }
+    }
+    igraph_vector_resize(&vedges, it);
+    igraph_add_edges(graph, &vedges, 0);
+    igraph_vector_destroy(&vedges);
+}
+
+
 int main() {
     printf("Hello, World!\n");
 
-    // initializing variables
     igraph_t graph;
+    igraph_t spanner;
 
+    // reading the file
     FILE *gfile;
-    gfile = fopen("graph_easy.txt","r");
+    gfile = fopen("/home/martin/Data/VLG/graph_easy.txt","r");
+    if(igraph_read_graph_edgelist(&graph, gfile, 0, IGRAPH_UNDIRECTED))
+        printf("rip\n");
 
+    // initializing variables
+    igraph_empty(&spanner, igraph_vcount(&graph), IGRAPH_UNDIRECTED);
 
-
-    if(!igraph_read_graph_edgelist(graph, gfile, 0, IGRAPH_UNDIRECTED))
-        printf("rip\n")
-
-    igraph_vector_t vids;
+    // bfs
+    igraph_vector_t vids, layers, parents;
     igraph_vector_init(&vids, 0);
-    igraph_vector_t parents;
+    igraph_vector_init(&layers, 0);
     igraph_vector_init(&parents, 0);
 
     igraph_bfs_simple(&graph,
-                      1,
-                      0,
+                      5,
+                      IGRAPH_ALL,
                       &vids,
-                      0,
+                      &layers,
                       &parents);
 
-    print_vector(&vids);
-    print_vector(&parents);
+    printf("BFS:\n");
+    vector_print(&vids);
+    vector_print(&layers);
+    vector_print(&parents);
+    printf("\n");
+
+    // merge
+    printf("MERGE:");
+    merge(&spanner, &parents);
+    graph_print(&spanner);
 
     // destroy the objects
-
-    fclose(gfile)
     igraph_destroy(&graph);
+    igraph_destroy(&spanner);
     igraph_vector_destroy(&vids);
+    igraph_vector_destroy(&layers);
+    igraph_vector_destroy(&parents);
+
+    fclose(gfile);
     return 0;
 }

66279730578d03ce1c03ddb3ed880d3223d2ce54
Vancothis <mathis.vancoppenolle@epita.fr>
2021-07-12 15:43:19 +0200

    add new metrics

--- main.c
+++ main.c
@@ -22,6 +22,34 @@ igraph_real_t max(igraph_real_t a, igraph_real_t b)
     return c;
 }
 
+igraph_real_t mean(igraph_vector_t *v)
+{
+    igraph_real_t sum = 0;
+    for (igraph_integer_t i = 0; i < igraph_vector_size(v); i++)
+        sum += VECTOR(*v)[i];
+    return sum/igraph_vector_size(v);
+}
+
+igraph_real_t var(igraph_vector_t *v, igraph_real_t m)
+{
+    igraph_real_t sum = 0;
+    for (igraph_integer_t i = 0; i < igraph_vector_size(v); i++)
+        sum += VECTOR(*v)[i] * VECTOR(*v)[i];
+    return (sum/igraph_vector_size(v)) - (m * m);
+}
+
+
+igraph_real_t max_vec(igraph_vector_t *v)
+{
+    igraph_real_t m = VECTOR(*v)[0];
+    for (igraph_integer_t i = 1; i < igraph_vector_size(v); i++)
+        if (m < VECTOR(*v)[i])
+	    m = VECTOR(*v)[i];
+    return m;
+}
+
+
+
 void read_file(igraph_t *graph)
 {
     printf("reading...\n");
@@ -71,37 +99,43 @@ void get_subset(igraph_vector_t *vertices, igraph_t *graph)
     igraph_vector_resize(vertices, k);
 }
 
-void merge(igraph_t *graph, igraph_vector_t *parents)
+void merge(igraph_t *graph, igraph_vector_t *parents, igraph_vector_t *vdeg)
 {
     /// XXX uses a lot of RAM by allocationg a vedges
     igraph_vector_t vedges;
     igraph_vector_init(&vedges, igraph_vector_size(parents) * 2);
+    
+    igraph_vector_t neighbors;
+    igraph_vector_init(&neighbors, 0);
 
     long int it = 0;
     for (igraph_integer_t i = 0; i < igraph_vector_size(parents); i++)
     {
         igraph_integer_t p_i = VECTOR(*parents)[i];
-        igraph_vector_t neighbors;
-        igraph_vector_init(&neighbors, 0);
-        igraph_neighbors(graph, &neighbors, i, IGRAPH_ALL);
-        unsigned stop = 0;
-        if (i == p_i)
+	if (i == p_i)
             continue;
-        for (long int j = 0; j < igraph_vector_size(&neighbors); j++)
+        igraph_neighbors(graph, &neighbors, i, IGRAPH_ALL);
+
+	//get the node degree metric
+        VECTOR(*vdeg)[i] = igraph_vector_size(&neighbors); 
+        unsigned stop = 0;
+	for (long int j = 0; j < igraph_vector_size(&neighbors); j++)
         {
             if (p_i == VECTOR(neighbors)[j])
             {
-                stop = 1;
+		stop = 1;
                 break;
             }
         }
-        if (!stop)
-        {
+	if (!stop)
+	{
             VECTOR(vedges)[it] = i;
             it += 1;
             VECTOR(vedges)[it] = p_i;
             it += 1;
-        }
+            //add the new edge in the degree count
+	    VECTOR(*vdeg)[i]++;
+	}
     }
     igraph_vector_resize(&vedges, it);
     igraph_add_edges(graph, &vedges, 0);
@@ -118,9 +152,10 @@ int main() {
 
     igraph_vector_bool_t eccbool;
 
-    igraph_vector_t eccmin, eccmax, ecc;
+    igraph_vector_t eccmin, eccmax, ecc, eccdelta;
  
-
+    //vertices degrees
+    igraph_vector_t vdeg;
 
     igraph_vector_t vids, layers, parents;
     igraph_vector_init(&vids, 0);
@@ -146,9 +181,11 @@ int main() {
     igraph_vector_init(&eccmax, igraph_vcount(&graph));
     igraph_vector_fill(&eccmax, DBL_MAX);
     
+    igraph_vector_init(&vdeg, igraph_vcount(&graph));
+
     igraph_vector_init(&ecc, igraph_vcount(&graph));   
 
-     
+    igraph_vector_init(&eccdelta, igraph_vcount(&graph));
 
     // (3) for s in S
     for (igraph_integer_t i = 0; i < igraph_vector_size(&subset); i++)
@@ -175,7 +212,7 @@ int main() {
 
 
         // (3.b) merging with the spanner
-        merge(&spanner, &parents);
+        merge(&spanner, &parents, &vdeg);
 
 
 
@@ -187,7 +224,7 @@ int main() {
         VECTOR(eccmin)[v] = igraph_vector_size(&layers) - 2;
         VECTOR(eccmax)[v] = igraph_vector_size(&layers) - 2;
 	VECTOR(eccbool)[v] = 1;
-
+        VECTOR(eccdelta)[v] = 0;
 	
 	igraph_integer_t di = 1;
 	igraph_real_t d = 0;
@@ -205,31 +242,32 @@ int main() {
               di++;
 	      d++;
 	    }
-            printf("w = %u, bw = %u, d = %f\n", w, VECTOR(eccbool)[w], d);
+            //printf("w = %u, bw = %u, d = %f\n", w, VECTOR(eccbool)[w], d);
            
 	    if(!VECTOR(eccbool)[w])
 	    {
                 VECTOR(eccmin)[w] = max(VECTOR(eccmin)[w], max(VECTOR(ecc)[v] - d, d));
 
 	        VECTOR(eccmax)[w] = min(VECTOR(eccmax)[w], VECTOR(ecc)[v] + d);
-		if(VECTOR(eccmax)[w] == VECTOR(eccmin)[w])
+		VECTOR(eccdelta)[w] = VECTOR(eccmax)[w] - VECTOR(eccmin)[w];
+		if(VECTOR(eccdelta)[w] == 0)
 		{
-		    VECTOR(ecc)[w] = VECTOR(eccmax)[w];
-		    VECTOR(eccbool)[w] = 1; 
+		    VECTOR(ecc)[w] = VECTOR(eccmax)[w]; 
+	            VECTOR(eccbool)[w] = 1; 
 		}
 	    }
+	    
 	}
- 
-
-
-
-
-
-
-
-
-
-    
+	printf("DELTA: \n");
+	vector_print(&eccdelta);
+        igraph_real_t meandelta = mean(&eccdelta);
+	igraph_real_t vardelta = var(&eccdelta, meandelta);
+	printf("mean = %f\n", meandelta);
+	printf("var = %f\n", vardelta);
+	printf("DEGREE:\n");
+	vector_print(&vdeg);
+        igraph_real_t meandeg = mean(&vdeg);
+	printf("mean deg = %f\n", meandeg);
     }
     printf("SPANNER\n");
     graph_print(&spanner);
@@ -241,7 +279,7 @@ int main() {
     vector_print(&ecc);
     vector_bool_print(&eccbool);
 
-
+    printf("maxecc = %f\n", max_vec(&eccmax));
 
     // destroying the bfs objects
     igraph_vector_destroy(&vids);
@@ -250,6 +288,10 @@ int main() {
     igraph_vector_destroy(&eccmin);
     igraph_vector_destroy(&eccmax);
     igraph_vector_destroy(&ecc);
+    igraph_vector_destroy(&eccdelta);
+    igraph_vector_destroy(&vdeg);
+ 
+
     igraph_vector_bool_destroy(&eccbool);
     
     
